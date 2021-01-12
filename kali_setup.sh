dfe0aa6739f1d4f217a894be4f2b7c7223b7224e
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-01-12 10:00:24 +0000

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -7,27 +7,27 @@
 # create user to not require password for sudo [sudo visudo / theUSER ALL=(ALL) NOPASSWD:ALL)
 # wallpaper: https://raw.githubusercontent.com/D4nk0St0rM/simple_linux_tweaks/main/wallpaper/Kali_dark_shadow_eye.jpg
 
-echo -e "## Hold on to your hats.... cats"
+echo -e $fg[yellow]"## Hold on to your hats.... cats"$reset_color
 
 #set -x
 mytools="/opt/mytools"
 
-echo -e "update & upgrade"
+echo -e $fg[yellow]""update & upgrade"$reset_color
 
 sudo apt-get update && apt-get upgrade
 sudo apt-get dist-upgrade -y
 
-echo -e "~#~ GB Keyboard"
+echo -e $fg[yellow]""~#~ GB Keyboard"$reset_color
 sudo setxkbmap -layout gb
 
-echo -e "~#~ Terminal config for looks"
+echo -e $fg[yellow]"~#~ Terminal config for looks"$reset_color
 ### see here for further config changes: https://github.com/D4nk0St0rM/simple_linux_tweaks/blob/main/konsole_zsh_terminal.md
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 
-echo -e "~#~ update and install some fundamental tools for running environment"
+echo -e $fg[yellow]"~#~ update and install some fundamental tools for running environment"$reset_color
 sudo apt-get install -y git
 # sudo apt-get install -y python3.7 python3-pip python2.7 python-pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -47,7 +47,7 @@ sudo apt-get install dialog -y
 sudo apt-get install protonvpn-cli -y
 
 
-echo -e "~#~ Some structure changes, deleting and creating folders, & virtual python envs"
+echo -e $fg[yellow]"~#~ Some structure changes, deleting and creating folders, & virtual python envs"$reset_color
 rm -r Music/
 rm -r Templates/
 rm -r Videos/
@@ -67,7 +67,7 @@ virtualenv -p python3 python3
 virtualenv -p python2 python2
 
 
-echo -e "~#~ install a bunch of new tools"
+echo -e $fg[yellow]"~#~ install a bunch of new tools"$reset_color
 sudo apt-get install joplin -y
 sudo touch /etc/apt/sources.list.d/insync.list
 ### MANUPDATE : sudo echo "deb http://apt.insync.io/debian buster non-free contrib" > /etc/apt/sources.list.d/insync.list
@@ -76,35 +76,35 @@ sudo apt-get update
 sudo apt-get install insync -y
 
 
-echo -e "~#~ install a bunch git clones to /opt/mytools"
-echo -e "~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~"
-echo -e '~#~ SecLists [now kali install]'
+echo -e $fg[yellow]"~#~ install a bunch git clones to /opt/mytools"$reset_color
+echo -e $fg[red]"~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~ ~#~"$reset_color
+echo -e $fg[yellow]"~#~ SecLists [now kali install]"$reset_color
 sudo apt-get install seclists -y 
 
-echo -e '~#~ h8mail'
+echo -e $fg[yellow]"~#~ h8mail"$reset_color
 sudo git clone https://github.com/khast3x/h8mail $mytools/h8mail
-echo -e '~#~ discover'
+echo -e $fg[yellow]"~#~ discover"$reset_color
 sudo git clone https://github.com/leebaird/discover $mytools/discover
-echo -e '~#~ nmap automator'
+echo -e $fg[yellow]"~#~ nmap automator"$reset_color
 sudo git clone https://github.com/21y4d/nmapAutomator.git $mytools/nmapAutomator
-echo -e '~#~ subbrute'
+echo -e $fg[yellow]"~#~ subbrute"$reset_color
 sudo git clone https://github.com/TheRook/subbrute.git $mytools/subbrute
-echo -e '~#~ theHarvester'
+echo -e $fg[yellow]"~#~ theHarvester"$reset_color
 sudo git clone https://github.com/laramies/theHarvester.git $mytools/theHarvester
-echo -e '~#~ windows exploit suggester'
+echo -e $fg[yellow]"~#~ windows exploit suggester"$reset_color
 sudo git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git $mytools/windows-exploit-suggester
-echo -e '~#~ nmap vulners'
+echo -e $fg[yellow]"~#~ nmap vulners"$reset_color
 sudo git clone https://github.com/vulnersCom/nmap-vulners.git /usr/share/nmap/scripts/
-echo -e '~#~ priv esc scripts'
+echo -e $fg[yellow]"~#~ priv esc scripts"$reset_color
 sudo git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git $mytools/priv-esc-scripts
-echo -e '~#~ sublist3r'
+echo -e $fg[yellow]"'~#~ sublist3r"$reset_color
 sudo git clone https://github.com/aboul3la/Sublist3r.git $mytools/sublist3r
-echo -e '~#~ sherlock'
+echo -e $fg[yellow]"~#~ sherlock"$reset_color
 git clone https://github.com/sherlock-project/sherlock.git $mytools/sherlock
 $ python3 -m pip install -r $mytools/sherlock/requirements.txt
-echo -e '~#~ windows reverse shell'
+echo -e $fg[yellow]"~#~ windows reverse shell"$reset_color
 git clone https://github.com/Dhayalanb/windows-php-reverse-shell.git $mytools/windows-reverse-shell
-echo -e '~#~ one-liner'
+echo -e $fg[yellow]"~#~ one-liner"$reset_color
 git clone https://github.com/D4Vinci/One-Lin3r.git $mytools/one-liner
 sudo apt install libncurses5-dev
 sudo pip3 install .$mytools/one-liner/One-Lin3r --user
