0fb7ec38d67b593384b4a43a943ab3c95005da83
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-02-13 11:04:09 -0500

    Update _new_kali_install_.sh

--- _new_kali_install_.sh
+++ _new_kali_install_.sh
@@ -65,7 +65,21 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bakup
 sudo cp sources.list /etc/apt/sources.list
 rm sources.list
 
+#### add sources for visual studio
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install apt-transport-https
 sudo apt-get update
+
+####install visual code studio
+echo -e "${RED}==============================================================${NC}"
+echo -e "${RED}==============================================================${NC}"
+echo -e "${GREEN}======== Install Microsoft Visual Code Studio ================${NC}"
+echo -e "${RED}==============================================================${NC}"
+echo -e "${RED}==============================================================${NC}"
+sudo apt install code
+
 
 #### Tweaking the themeing and look
 echo -e "${RED}==============================================================${NC}"
